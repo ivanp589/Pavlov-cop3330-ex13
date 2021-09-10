8c44f0ee6d9a0cecaa1c10fb372fe4d5a51a9d59
ivanp589 <ivanpav@Knights.ucf.edu>
2021-09-10 12:39:45 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static Scanner in = new Scanner(System.in);
+    public static void main( String[] args )
+    {
+
+      //  System.out.println( "Hello World!" );
+        App math = new App();
+        App ask = new App();
+        double prin = ask.amount();
+        double rate = ask.rate();
+        int years = ask.years();
+        int times = ask.comp();
+        double total1 = math.calc(rate,years, prin, times);
+        double total = math.rounds(total1);
+        out.println(String.format("$%.2f invested at %.2f " +
+                "for %d years compounded %d times per year is $%.2f.",prin,rate,years, times, total));
+        //out.println(String.format("%f ",total));
+    }
+
+    private double amount(){
+        out.print("What is the principal amount? ");
+        return in.nextDouble();
+    }
+    private double rate(){
+        out.print("What is the rate? ");
+        return in.nextDouble()/100;
+    }
+    private int years(){
+        out.print("What is the number of years? ");
+        return in.nextInt();
+    }
+    private int comp(){
+        out.print("What is the number of times the interest is compounded per year? ");
+        return in.nextInt();
+    }
+    private double calc(double rate,double years,double prin,double times){
+        double sum = 1;
+
+        for(int x=0;x<(times*years);x++){
+            sum *=(1+(rate)/times);
+        }
+        double calc = prin*(sum);
+        out.println(String.format("this is  calc %f ", calc));
+        return calc;
+
+    }
+    private double rounds( double total1){
+        double roundOff=Math.ceil(total1*100)/100;
+        return roundOff;
+    }
+}
